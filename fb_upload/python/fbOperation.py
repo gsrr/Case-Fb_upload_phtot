451fb6208ada45bf39aa68b9ba8f632ac3227fe7
gsrr <jerrycheng1128@gmail.com>
2015-10-31 18:07:30 +0800

    update

--- fb_upload/python/fbOperation.py
+++ fb_upload/python/fbOperation.py
@@ -18,15 +18,33 @@ graph = GraphAPI(oauth_access_token)
     # message = "bbbbbbbbbbb"
 # )
 
+def fetch_album(url_album):
+    searchObj = re.search(r'set=oa\.(.*?)\&type=1', url_album)
+    return searchObj.group(1)
+    
+def upload_photo(paras):
+    oauth_access_token = paras['token']
+    id_album = fetch_album(paras['url_album'])
+    src1 = paras['src1'].lstrip("/")
+    src2 = paras['src2']
+    desc = paras['desc']
+    graph = GraphAPI(oauth_access_token)
+    graph.post(
+        path = '%s/photos'%id_album,
+        source = open(src1, 'rb'),
+        message = desc
+    )
+    print {'status' : 0}
+
 def fetch_group(url_group):
     searchObj = re.search(r'groups/(.*?)/', url_group)
     return searchObj.group(1)
 
 def create_album(paras):
+    oauth_access_token = paras['token']
     id_group = fetch_group(paras['id_group'])
     name = paras['name_album']
     message = paras['msg_album']
-    oauth_access_token = "CAACEdEose0cBAEimKmZBgRmlTr3mnVrmjvkeBg1kLAjFZC3obUP9T1oZA7zI0b0xl0ig5qIrxEGKVuodkXxYmJfSgsqmPUrVsFOVFROFAGhdHdyRZBoifwZAZAgZCpUqo2Kv4SJgXptZAxQnzbZAlKFjd0wbJsZCxiKWlWt3EA4tddZBkjtzUQGWtCrCjZBg2WrsEU4zS54zgpWEppKi2jcWpxzy"
     graph = GraphAPI(oauth_access_token)
     graph.post(
         path = '/%s/albums'%id_group,
