7352de6926f66c72b9321b6f8339c1b58135f6b6
gsrr <jerrycheng1128@gmail.com>
2015-11-02 00:32:24 +0800

    update

--- fb_upload/js/callPython.js
+++ fb_upload/js/callPython.js
@@ -1,11 +1,13 @@
 
 
-function callPython(paras,func)
+function callPython(paras,func, async)
 {
+    async = typeof async !== 'undefined' ? async : true;
     $.ajax({
         type: "POST",
         dataType: "json",
-        //url: "/channel.php",
+        async: async,
+        //url: "http://127.0.0.1/channel.php",
         url: "/channel.php",
         data: paras,
         success: function(ret) {;
